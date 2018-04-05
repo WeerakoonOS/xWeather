ec18bed4ca92e233689e689236ac7f1cef431820
WeerakoonOS <osw0294@gmail.com>
2018-04-05 21:08:08 +0530

    api call update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
+
+const apiKey = 'eb59071d486d6c9ef2159f4c067b5fee';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,8 +15,24 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   //res.send('Hello World!')
-  console.log(req.body.city);
-  res.render('index');
+  //console.log(req.body.city);
+  //res.render('index');
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){// user invalid input
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
