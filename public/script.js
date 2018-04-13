5bcbd5e9db95966538187312a1b43e864cde089a
WeerakoonOS <osw0294@gmail.com>
2018-04-13 16:19:48 +0530

    script tag for nav bar icon

---
+++ public/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
